b9721a88c8e58e35d5e38ad5885caf9513a5ea61
MD.SUJAN <csesujon155@gmail.com>
2022-12-19 08:20:37 +0600

    Admin Template Mastering

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,3 +1,21 @@
-<x-app-layout>
-    
-</x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Corona Admin</title>
+    @include('admin.css')
+  </head>
+  <body>
+    <div class="container-scroller">
+      <!-- partial:partials/_sidebar.html -->
+        @include('admin.sidebar')
+      <!-- partial -->
+        @include('admin.header')
+        <!-- partial -->
+        @include('admin.body')
+    <!-- container-scroller -->
+    @include('admin.script')
+  </body>
+</html>
